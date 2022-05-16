12c5c882d12bbeedf796b72241c049359235e1ec
Miguel Quezada <mig.aquezada@gmail.com>
2022-05-15 23:39:25 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Miguel Quezada on 15-05-22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
